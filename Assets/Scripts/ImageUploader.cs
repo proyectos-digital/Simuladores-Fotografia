4779aae65c102632a50d937fb4884cee30ae1369
Rafael Florez <programador1@booksandbooksdigital.com.co>
2023-07-10 12:36:44 -0500

    cambios escena testing

---
+++ Assets/Scripts/ImageUploader.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using UnityEngine.Events;
+
+public enum ImageType{
+	PNG,
+	JPG
+}
+
+public class ImageUploader : MonoBehaviour{
+	Texture2D imageTexture;
+	string fieldName;
+	string fileName = "defaultImageName";
+	ImageType imageType = ImageType.JPG;
+	string url;
+	byte[] bytes;
+
+	//Events
+	UnityAction<string> OnErrorAction;
+	UnityAction<string> OnCompleteAction;
+
+	public static ImageUploader Initialize (){
+		return new GameObject ("ImageUploader").AddComponent <ImageUploader> ();
+	}
+
+	public ImageUploader SetUrl (string serverUrl){
+		this.url = serverUrl;
+		return this;
+	}
+
+	public ImageUploader SetTexture (Texture2D texture){
+		this.imageTexture = texture;
+		return this;
+	}
+	public ImageUploader SetBytes(byte[] bytes){
+		this.bytes = bytes;
+		return this;
+	}
+
+	public ImageUploader SetFileName (string filename){
+		this.fileName = filename;
+		return this;
+	}
+
+	public ImageUploader SetFieldName (string fieldName){
+		this.fieldName = fieldName;
+		return this;
+	}
+
+	public ImageUploader SetType (ImageType type){
+		this.imageType = type;
+		return this;
+	}
+	//events
+	public ImageUploader OnError (UnityAction<string> action){
+		this.OnErrorAction = action;
+		return this;
+	}
+
+	public ImageUploader OnComplete (UnityAction<string> action){
+		this.OnCompleteAction = action;
+		return this;
+	}
+
+	public void Upload (){
+		//check/validate fields
+		if (url == null)
+			Debug.LogError ("Url is not assigned, use SetUrl( url ) to set it. ");
+		//...other checks...
+		//...
+		StopAllCoroutines ();
+		StartCoroutine (StartUploading ());
+	}
+	IEnumerator StartUploading (){
+		WWWForm form = new WWWForm ();
+		byte[] textureBytes = null;
+
+        //Get a copy of the texture, because we can't access original texure data directly. 
+        //Texture2D imageTexture_copy = GetTextureCopy (imageTexture);
+
+        switch (imageType){
+            case ImageType.PNG:
+                textureBytes = imageTexture.EncodeToPNG();
+                break;
+            case ImageType.JPG:
+                textureBytes = imageTexture.EncodeToJPG();
+                break;
+        }
+
+        //image file extension
+        string extension = imageType.ToString ().ToLower ();
+
+		form.AddBinaryData (fieldName, textureBytes, fileName + "." + extension, "image/" + extension);
+
+		//WWW w = new WWW (url, form);
+		UnityWebRequest webRequest = UnityWebRequest.Post(url, form);
+
+		//yield return w;
+		yield return webRequest.SendWebRequest();
+
+		if (webRequest.error != null) {
+			//error : 
+			if (OnErrorAction != null)
+				OnErrorAction(webRequest.error); //or OnErrorAction.Invoke (w.error);
+		} else {
+			//success
+			if (OnCompleteAction != null)
+				OnCompleteAction(webRequest.downloadHandler.text); //or OnCompleteAction.Invoke (w.error);
+		}
+		webRequest.Dispose ();
+		Destroy (this.gameObject);
+	}
+
+	Texture2D GetTextureCopy (Texture2D source){
+		//Create a RenderTexture
+		RenderTexture rt = RenderTexture.GetTemporary (
+			                   source.width,
+			                   source.height,
+			                   0,
+			                   RenderTextureFormat.Default,
+			                   RenderTextureReadWrite.Linear
+							   );
+
+		//Copy source texture to the new render (RenderTexture) 
+		Graphics.Blit (source, rt);
+
+		//Store the active RenderTexture & activate new created one (rt)
+		RenderTexture previous = RenderTexture.active;
+		RenderTexture.active = rt;
+
+		//Create new Texture2D and fill its pixels from rt and apply changes.
+		Texture2D readableTexture = new Texture2D (source.width, source.height);
+		readableTexture.ReadPixels (new Rect (0, 0, rt.width, rt.height), 0, 0);
+		readableTexture.Apply ();
+
+		//activate the (previous) RenderTexture and release texture created with (GetTemporary( ) ..)
+		RenderTexture.active = previous;
+		RenderTexture.ReleaseTemporary (rt);
+
+		return readableTexture;
+	}
+}
---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+using TMPro;
+
+public class Screenshot : MonoBehaviour
+{
+    public int resWidth = 0;
+    public int resHeight = 0;
+    public Camera mainCamera;
+
+    private bool takeHiResShot = false;
+
+    public PhotoInventory photoInventory;
+    public string fieldName;
+    public string serverUrl;
+
+    public GameObject luzFlash;
+    public TMP_Text txtItem;
+    public enum ImageFormat {
+        jpg,
+        png
+    }
+
+    public int screenshotUpscale = 1;
+
+    public static string ScreenShotName(int width, int height){
+        if (!Directory.Exists(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+"/Screenshots")) {
+            Directory.CreateDirectory(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Screenshots");
+        }
+        return string.Format("{0}/Screenshots/screen_{1}x{2}_{3}.png",
+            System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+            width, height,
+            System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void TakeHiResShot()
+    {
+        takeHiResShot = true;
+    }
+
+    void LateUpdate() {
+        //takeHiResShot |= Input.GetKeyUp(KeyCode.P); //Input.GetKeyDown("k");
+        if (Input.GetKeyUp(KeyCode.K) || takeHiResShot) {
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            mainCamera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            mainCamera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            mainCamera.targetTexture = null;
+            RenderTexture.active = null;
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string fileName = ScreenShotName(resWidth, resHeight);
+            System.IO.File.WriteAllBytes(fileName, bytes);
+            Debug.Log("fileName: " + fileName);
+            txtItem.text = "se tomo la foto: "+fileName;
+            Debug.Log(string.Format("Took screenshot to: {0}", fileName));
+            takeHiResShot = false;
+
+        }
+    }
+
+    public void GetScreenshot() {
+        //Iluminar luz con flash
+        luzFlash.SetActive(true);
+        takeHiResShot = true;
+
+        StartCoroutine("FlashOff");
+        //_webGLDownload.GetScreenshot(WebGLDownload.ImageFormat.jpg, 1, "");
+        //if (!takeHiResShot) StartCoroutine(RecordUpscaledFrame(ImageFormat.jpg, screenshotUpscale, ""));
+    }
+
+    IEnumerator RecordUpscaledFrame(ImageFormat imageFormat, int screenshotUpscale, string fileName) {
+        takeHiResShot = true;
+        yield return new WaitForEndOfFrame();
+        try {
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            mainCamera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            mainCamera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            mainCamera.targetTexture = null;
+            //RenderTexture.active = null;
+            //Destroy(rt);
+            //byte[] bytes = screenShot.EncodeToJPG();
+            string dateFormat = "yyyy-MM-dd-HH-mm-ss";
+            fileName = resWidth.ToString() + "x" + resHeight.ToString() + "px_" + System.DateTime.Now.ToString(dateFormat);
+            photoInventory.GetTexture(rt);
+            RenderTexture.active = null;
+            if (fileName == "") {
+                int resWidth = mainCamera.pixelWidth * screenshotUpscale;
+                int resHeight = mainCamera.pixelHeight * screenshotUpscale;
+            }
+            //Texture2D screenShot = ScreenCapture.CaptureScreenshotAsTexture(screenshotUpscale);
+            //ImageUploader.Initialize()
+            //    .SetUrl(serverUrl)
+            //    .SetTexture(screenShot)
+            //    //.SetBytes(bytes)
+            //    .SetFieldName(fieldName)
+            //    .SetFileName(fileName)
+            //    .SetType(ImageType.JPG)
+            //    .OnError(error => Debug.Log(error))
+            //    .OnComplete(text => Debug.Log(text))
+            //    .Upload();
+            //if (imageFormat == ImageFormat.jpg) DownloadFile({{..EncodeToJPG(), fileName, "jpg");
+            //else if (imageFormat == ImageFormat.png) DownloadFile(screenShot.EncodeToPNG(), fileName, "png");
+            Destroy(screenShot);
+        } catch (System.Exception e) {
+            Debug.Log("Original error: " + e.Message);
+        }
+        takeHiResShot = false;
+        StartCoroutine("FlashOff");
+    }
+    IEnumerator FlashOff() {
+        yield return new WaitForSeconds(0.5f);
+        luzFlash.SetActive(false);
+    }
+}
