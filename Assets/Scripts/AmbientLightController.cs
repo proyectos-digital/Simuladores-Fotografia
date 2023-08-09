578862eb66b186cac2e5b762c6408b73a8d0ed77
Jaime Andres Ibarra <programadorweb2@booksandbooksdigital.com.co>
2023-08-09 15:01:25 -0500

    feat(Ajuste iluminacion escenario, Atardecer y Anochecer)

--- Assets/Scripts/AmbientLightController.cs
+++ Assets/Scripts/AmbientLightController.cs
@@ -7,12 +7,13 @@ public class AmbientLightController : MonoBehaviour {
     [SerializeField] private Light ambientLight;
     [SerializeField] private Light[] lampLights;
 
-    public Material dayMaterial, nightMaterial;
+    public Material dayMaterial, nightMaterial,sunriseMaterial,sunsetMaterial;
 
     public void ChangeDay(float time) {
+        DisactivateLamps();
         //ambientLight.transform.Rotate(new Vector3(25, 0f, 0f));
-        ambientLight.transform.localEulerAngles = new Vector3(time, ambientLight.transform.localEulerAngles.y, ambientLight.transform.localEulerAngles.z);
-        if (time > 155) {
+        //ambientLight.transform.localEulerAngles = new Vector3(time, ambientLight.transform.localEulerAngles.y, ambientLight.transform.localEulerAngles.z);
+        /*if (time > 155) {
             RenderSettings.skybox = nightMaterial;
             RenderSettings.ambientIntensity = 0.5f;
             ActivateLamps();
@@ -20,9 +21,40 @@ public class AmbientLightController : MonoBehaviour {
             DisactivateLamps();
             RenderSettings.skybox = dayMaterial;
             RenderSettings.ambientIntensity = 1f;
+        }*/
+        switch (time)
+        {
+            case 1:
+                RenderSettings.skybox = sunriseMaterial;
+                DisactivateLamps();
+                RenderSettings.fog = true;
+                RenderSettings.fogColor = new Color32(0x6C, 0x86, 0x9C, 0xff);
+                RenderSettings.fogDensity = 0.023f;
+                break;
+            case 2:
+                RenderSettings.skybox = dayMaterial;
+                DisactivateLamps();
+                break;
+            case 3:
+                RenderSettings.skybox = sunsetMaterial;
+                RenderSettings.fog = true;
+                RenderSettings.fogColor = new Color32(0x67,0x4E,0x28,0xff);
+                RenderSettings.fogDensity = 0.023f;
+                ActivateLamps();
+                break;
+            case 4:
+                RenderSettings.skybox = nightMaterial;
+                RenderSettings.fog = true;
+                RenderSettings.fogColor = new Color32(0x25, 0x29, 0x35, 0xff);
+                RenderSettings.fogDensity = 0.04f;
+                ActivateLamps();
+                break;
         }
     }
 
+    public void ChangeIntensity(float value){
+        ambientLight.intensity = value;
+    }
     public void ChangeTemperature(float value) {
         ambientLight.colorTemperature = value;
     }
@@ -31,7 +63,7 @@ public class AmbientLightController : MonoBehaviour {
         foreach(Light light in lampLights) {
             light.gameObject.SetActive(true);
         }
-        ambientLight.gameObject.SetActive(false);
+        //ambientLight.gameObject.SetActive(false);
     }
     void DisactivateLamps() {
         foreach (Light light in lampLights) {
