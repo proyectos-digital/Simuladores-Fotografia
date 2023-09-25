9e3ceaac657a635330ace99d81d5bb035cce1e5a
Rafael Florez <programador1@booksandbooksdigital.com.co>
2023-09-25 14:46:14 -0500

    mover objetos por rigidbody

--- Assets/Scripts/TomaElementos.cs
+++ Assets/Scripts/TomaElementos.cs
@@ -1,52 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TomaElementos : MonoBehaviour
-{
-    public GameObject elementos;
-    private Transform posicionElemento; //Mano
-    private bool activ; //Para saber cuando estoy dentro o fuera de la zona del objeto
+public class TomaElementos : MonoBehaviour {
+    [Header("Toma de elementos")]
+    public TMP_Text txtAviso;
+    public GameObject elementos;            //El elemento que tomaré
+    private Transform posicionElemento;     //Mano
+    private bool activ;                     //Para saber cuando estoy dentro o fuera de la zona del objeto
+    Rigidbody rb;
 
-    private void Start(){
+    private void Start() {
         GameObject objetoMano = GameObject.FindWithTag("Mano");
+        rb = GetComponent<Rigidbody>();
 
-        if(objetoMano != null ){
+        if (objetoMano != null) {
             posicionElemento = objetoMano.transform;
-        } else{
-            Debug.LogWarning("No se encontró ningún objeto con la etiqueta Mano.");
+        } else {
+            Debug.LogError("No se encontró ningún objeto con la etiqueta Mano.");
         }
     }
-    void Update(){
+    void FixedUpdate() {
         TomaElemento();
     }
 
-    public void TomaElemento(){
-        if (activ == true){
+    public void TomaElemento() {
+        if (activ) {
             //Toma el elemento
-            if (Input.GetKeyDown(KeyCode.Q)){
-                elementos.transform.SetParent(posicionElemento); //El elemento se colocará dentro del objeto que esta en el player
-                elementos.transform.position = posicionElemento.position; //Le digo que el elemento debe quedar en la misma posición que el padre
-                //videoCam.GetComponent<Rigidbody>().isKinematic = true;
-            }
+            if (Input.GetKeyDown(KeyCode.Q) && posicionElemento.childCount == 0) {
+                elementos.transform.SetParent(posicionElemento);            //El elemento se colocará dentro del objeto que esta en el player
+                //elementos.transform.position = posicionElemento.position;   //Le digo que el elemento debe quedar en la misma posición que el padre
 
-            //Suelta el elemento
-            if (Input.GetKeyDown(KeyCode.E)){
-                elementos.transform.SetParent(null);    
-                //videoCam.GetComponent<Rigidbody>().isKinematic = false;
+                //elementos.transform.rotation = posicionElemento.rotation;
+                rb.isKinematic = false;
+                rb.detectCollisions = true;
+                rb.MovePosition(posicionElemento.localPosition);
+               
+            } else if (Input.GetKeyDown(KeyCode.Q) && posicionElemento.childCount > 0) {
+                StartCoroutine(TextoAviso());
             }
+        }
+        //Suelta el elemento
+        if (Input.GetKeyDown(KeyCode.E) && posicionElemento.childCount > 0) {
+            elementos.transform.SetParent(null);
+            rb.isKinematic = true;
+            //rb.detectCollisions = false;
         }
     }
 
+    IEnumerator TextoAviso() {
+        txtAviso.text = "Toma de a un objeto!";
+        yield return new WaitForSeconds(2f);
+        txtAviso.text = "";
+    }
+
     private void OnTriggerEnter(Collider other) {
-        if(other.tag == "Player"){
+        if (other.tag == "Mano") {
+            Debug.Log("entro");
             activ = true;
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        if(other.tag == "Player"){
+        if (other.tag == "Mano") {
             activ = false;
+            Debug.Log("me sali: "+activ);
         }
     }
 }
