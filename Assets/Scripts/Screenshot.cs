1b7c8ca6f7bd3a909a1b00d18cadb3d1cdeeb57e
Jaime Andres Ibarra <programadorweb2@booksandbooksdigital.com.co>
2024-06-14 15:18:57 -0500

    Ajustes de Audio y Notificaciones

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -11,7 +11,8 @@ public class Screenshot : MonoBehaviour
     [SerializeField] int resWidth = 0;
     [SerializeField] int resHeight = 0;
     [SerializeField] Camera mainCamera;
-
+    public AudioSource cameraSound;
+    bool m_Play;
     private bool takeHiResShot = false, isHorizontal = true, isFlashing = false;
 
     //public PhotoInventory photoInventory;
@@ -40,6 +41,12 @@ public class Screenshot : MonoBehaviour
 
     public void TakeHiResShot(){
         takeHiResShot = true;
+    }
+
+    public void Start()
+    {
+        cameraSound = GetComponent<AudioSource>();
+        m_Play = false;
     }
     public void ChangeOrientation() {
         isHorizontal = !isHorizontal;
@@ -92,6 +99,7 @@ public class Screenshot : MonoBehaviour
         //_webGLDownload.GetScreenshot(WebGLDownload.ImageFormat.jpg, 1, "");
         if (!takeHiResShot) StartCoroutine(TakeScreenshot(ImageFormat.jpg, screenshotUpscale));
     }
+
     IEnumerator TakeScreenshot(ImageFormat imageFormat, int screenshotUpscale) {
         takeHiResShot = true;
         yield return new WaitForEndOfFrame();
@@ -160,6 +168,7 @@ public class Screenshot : MonoBehaviour
         takeHiResShot = false;
         StartCoroutine("FlashOff");
     }*/
+
     IEnumerator FlashOff() {
         luzFlash.SetActive(true);
         yield return new WaitForSeconds(0.5f);
