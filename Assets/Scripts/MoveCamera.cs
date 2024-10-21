11e61f709a933a15f69bdbb4a4ca803d7ec6e4b0
Rafael Florez <programador1@booksandbooksdigital.com.co>
2024-10-21 06:35:38 -0500

    Refactorización scripts

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
@@ -12,6 +10,7 @@ public class MoveCamera : MonoBehaviour
     [SerializeField] CameraManager camManager;
 
     void Start() {
+        //Si no estamos en simulador Tv, busca asignamos la variable de correspondiente para el simulador fotografía
         if (!isTV)
         {
             camManager = GameObject.FindGameObjectWithTag("CamManager").GetComponent<CameraManager>();
@@ -20,12 +19,14 @@ public class MoveCamera : MonoBehaviour
     }
 
     void Update() {
+        //En escenas de Studio y Studio People cambiaremos la posición de la camara al entrar en modo cámara de esas escenas
         if (!isStudy) {
             transform.position = cameraPosition.position;
         } else {
             transform.position = studyPosition.position;
         }
     }
+    //Función para cambiar estado de variable y mover objeto al indicado.
     void MoveCam() {
         isStudy = !isStudy;
         transform.position = studyPosition.position;
--- Assets/Scripts/NotificationController.cs
+++ Assets/Scripts/NotificationController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -9,24 +7,19 @@ public class NotificationController : MonoBehaviour
     public GameObject notificationPanel;
     public TMP_Text notificationText;
     public Animation notificationAnimation;
-    // Start is called before the first frame update
+
+    //Asignamos el componente animation del panel de notificaciones para poder realizar los cambios
     void Start()
     {
         notificationAnimation = notificationPanel.GetComponent<Animation>();
     }
 
+    //Cuando es llamado muestra el mensaje en pantalla por unos segundos y luego se vuelve a ocultar
     public void SendNotification(string valueText)
     {
         notificationPanel.SetActive(true);
-        //Debug.Log(valueText);
         notificationAnimation.PlayQueued("Fade In", QueueMode.CompleteOthers);
         notificationText.text = valueText;
         notificationAnimation.PlayQueued("Fade Out", QueueMode.CompleteOthers);
-        //notificationAnimation.Play("Fade Out");
     }
-
-
-
-
-
 }
