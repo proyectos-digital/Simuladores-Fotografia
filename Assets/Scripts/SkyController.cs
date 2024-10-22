b70b87c75009aa739f27199f9eecdb7424cc013b
Rafael Florez <programador1@booksandbooksdigital.com.co>
2024-10-22 14:22:18 -0500

    controlador cielo

--- Assets/Scripts/SkyController.cs
+++ Assets/Scripts/SkyController.cs
@@ -1,37 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine;
 
+//Script encargada de manipular todo lo del global volume
 public class SkyController : MonoBehaviour
 {
-    public Volume skyVolume;
-    public GameObject sun;
-    private HDAdditionalLightData sunData;
-    private Light sunLight;
-    private VolumetricClouds volumetricClouds;
-    [SerializeField] public Material EmissionMaterial;
+    public Volume skyVolume; //Global volume
+    public GameObject sun; //Objeto luz sol
+    private HDAdditionalLightData sunData; //Componente data de luz HD
+    private Light sunLight; //Componente del objeto luz sol
+    private VolumetricClouds volumetricClouds; //Componente en skyVolume de las nubes volumetricas
+    [SerializeField] public Material EmissionMaterial; //Emitir luz en materiales
     [SerializeField] public Color LigthEmsvColor = new Color(1f, 0.8f, 0.5f, 1f);
-    public VolumetricClouds.CloudPresets[] cloudsPrefabs = { VolumetricClouds.CloudPresets.Overcast };
-    public VolumetricClouds.CloudPresets cloudPresetSelected;
+    public VolumetricClouds.CloudPresets[] cloudsPrefabs = { VolumetricClouds.CloudPresets.Overcast }; //Obtener los tipos de nubes predefinidas
+    public VolumetricClouds.CloudPresets cloudPresetSelected; //Tupo de nube a seleccionar
     private float emissiveIntensityNight = 7;
     private float emissiveIntensityDay = 0;
-    // Start is called before the first frame update
+    
+    //Inicializamos variables y llamamos función de nubes
     void Start()
     {
-        
         sunData = sun.GetComponent<HDAdditionalLightData>();
         sunLight = sun.GetComponent<Light>();
         SetCloudPreset();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    //Función para elegir un tipo de nubes aleatorio que se mantendra durante la ejecución del programa
     void SetCloudPreset() 
     {
         skyVolume.profile.TryGet<VolumetricClouds>(out volumetricClouds);
@@ -40,6 +33,7 @@ public class SkyController : MonoBehaviour
         volumetricClouds.cloudPreset = cloudPresetSelected;
     }
 
+    //Función para cambiar la hora del día: amanecer, mediodía, atardecer, noche
     public void SetTimeOfDay(float time)
     {
         switch (time)
@@ -62,7 +56,7 @@ public class SkyController : MonoBehaviour
                 break;
             case 3:
                 sun.transform.rotation = Quaternion.identity;
-                //mid sun
+                //sun set
                 sunData.intensity = 550f;
                 sun.transform.Rotate(175.0f, 0.0f, 0.0f, Space.Self);
                 EmissionMaterial.SetColor("_EmissiveColor", LigthEmsvColor * emissiveIntensityNight);
@@ -70,7 +64,7 @@ public class SkyController : MonoBehaviour
                 break;
             case 4:
                 sun.transform.rotation = Quaternion.identity;
-                //mid sun
+                //night
                 sunData.intensity = 1f;
                 sun.transform.Rotate(60.0f, 0.0f, 0.0f, Space.Self);
                 sunLight.colorTemperature = 15000f;
