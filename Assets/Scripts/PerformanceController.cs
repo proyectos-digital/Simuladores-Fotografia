6693cdb7ef8f9c924f792f76af5b56f0fc5cd1f7
Rafael Florez <programador1@booksandbooksdigital.com.co>
2025-02-06 06:58:46 -0500

    correccion en nosferatu, parcial materiales

--- Assets/Scripts/PerformanceController.cs
+++ Assets/Scripts/PerformanceController.cs
@@ -5,6 +5,7 @@ public class PerformanceController : MonoBehaviour
     //Ajustamos la velocidad de los frames por segundo a 60
     void Awake()
     {
+        QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 30;
     }
 
