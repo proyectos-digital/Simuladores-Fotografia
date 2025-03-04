35222eaebc94c4fd83bab8490c203e37ed088356
Rafael Florez <programador1@booksandbooksdigital.com.co>
2025-03-04 14:44:50 -0500

    correcciones menores

--- Assets/Scripts/InstanciarElementos.cs
+++ Assets/Scripts/InstanciarElementos.cs
@@ -144,6 +144,8 @@ public class InstanciarElementos : MonoBehaviour
                 {
                     btnMicrofonos[i].SetActive(false);
                 }
+                playerCam.MouseLocked();
+                tvController.PanelInventory();
             }
         }
     }
